ab4bf837973a2038b92981c8ff7ccd254986cf35
nekumelon <nekumelon@gmail.com>
2022-04-03 20:12:11 -0700

    Update header.py

--- scripts/header.py
+++ scripts/header.py
@@ -1,1 +1,12 @@
+import glob
 
+header = """/*\n\tauthor....: nekumelon\n\tLicense...: MIT (Check LICENSE)\n*/""";
+
+for filePath in glob.iglob('./**/*.js', recursive=True):
+    with open(filePath, 'r') as file:
+        contents = file.read();
+
+    if (header in contents): continue;
+    
+    with open(filePath, 'w') as file:
+        file.write(header + '\n\n' + contents);
