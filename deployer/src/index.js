36428bae939b76a96246d2e51af14cb4eb0405be
nekumelon <nekumelon@Cadens-MacBook-Pro.local>
2022-04-05 12:23:12 -0700

    deployer init

---
+++ deployer/src/index.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const { exec } = require('child_process');
+
+let lastSha;
+
+function deploy() {
+    exec('cd ../../back-end', () => {});
+    exec('git -am "heroku-deploy"', (err, stdout, stderr) => {
+        if (err || stderr) {
+            console.error(`A problem occured while committing the changes. ${err}`);
+
+            return;
+        }
+
+        console.log(stdout);
+    });
+
+    exec('git push heroku main', (err, stdout, stderr) => {
+        if (err || stderr) {
+            console.error(`A problem occured while pushing to heroku. ${err}`);
+
+            return;
+        }
+
+        console.log(stdout);
+    })
+}
+
+async function deploymentLoop() {
+    const commits = await axios.get('https://api.github.com/repos/nekumelon/CodeXR/commits');
+    const sha = commits.data[0].sha;
+
+    if (lastSha && sha !== lastSha) {
+        lastSha = sha;
+        console.log(`Hash change: ${sha} Deploying...`);
+        deploy();
+    }
+
+    setTimeout(deploymentLoop, 2 * 60 * 1000);
+}
+
+deploymentLoop();
