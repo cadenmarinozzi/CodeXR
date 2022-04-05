c89700bc29138368dbe0af9ce49f040587ced9cd
nekumelon <nekumelon@Cadens-MacBook-Pro.local>
2022-04-05 12:36:53 -0700

    deployer dir fix

--- deployer/src/index.js
+++ deployer/src/index.js
@@ -9,26 +9,15 @@ const { exec } = require('child_process');
 let lastSha;
 
 function deploy() {
-    exec('cd ../../back-end', () => {});
-    exec('git -am "heroku-deploy"', (err, stdout, stderr) => {
+    exec('cd ../../back-end && git commit -am "heroku-deploy" && git push heroku main', (err, stdout, stderr) => {
         if (err || stderr) {
-            console.error(`A problem occured while committing the changes. ${err}`);
+            console.error(`An error occured while deploying to heroku. ${err}`);
 
             return;
         }
 
         console.log(stdout);
     });
-
-    exec('git push heroku main', (err, stdout, stderr) => {
-        if (err || stderr) {
-            console.error(`A problem occured while pushing to heroku. ${err}`);
-
-            return;
-        }
-
-        console.log(stdout);
-    })
 }
 
 async function deploymentLoop() {
@@ -36,8 +25,17 @@ async function deploymentLoop() {
     const sha = commits.data[0].sha;
 
     if (lastSha && sha !== lastSha) {
-        console.log(`Hash change: ${sha} Deploying...`);
-        deploy();
+        const commit = await axios.get(`https://api.github.com/repos/nekumelon/CodeXR/commits/${sha}`);
+        let changed;
+
+        commit.files.forEach(file => {
+            if (commit.files.filename.includes('back-end/')) changed = true;
+        })
+
+        if (changed) {
+            console.log(`Hash change: ${sha} Deploying...`);
+            deploy();
+        }
     }
 
     lastSha = sha;
