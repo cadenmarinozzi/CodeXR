06172b8b2ee98c241ccaaa8c54f2a8415b793416
nekumelon <nekumelon@Cadens-MacBook-Pro.local>
2022-04-07 11:04:17 -0700

    site about changes

--- site/src/Components/AboutPage/AboutPage.js
+++ site/src/Components/AboutPage/AboutPage.js
@@ -13,12 +13,12 @@ class AboutPage extends React.Component {
                 <div className='column'>
                     <div className='row'>
                         <h1>How It Works</h1>
-                        <h3>CodeXR works by taking the current line of code in your text editor and querying it with the OpenAI Codex model to generate a completed code segment called a completion. It also provides context from the previous code in the document. <br /> <br /><span className='TLDR'>TL;DR: CodeXR uses AI to make worky.</span></h3>
+                        <h3>CodeXR works by taking the current line of code in your text editor and querying it with the OpenAI Codex model to generate a completed code segment called a completion. It also provides context code from the other code in the document. <br /> <br /><span className='TLDR'>TL;DR: CodeXR uses AI to make worky.</span></h3>
                     </div>
 
                     <div className='row'>
                         <h1>Accuracy</h1>
-                        <h3>CodeXR uses OpenAIs Cushman Codex, an extremely powerful code-focused AI, trained specifically to be able to read and interpret code. BUT, the Codex is also trained on Natural Language, things such as Wikipedia articles, papers and texts, so results should be verbose. However, Codex isn't perfect, so once in a while you may get some unexpected code. If this happens, rewrite your prompt, making sure it provides just enough information that the Codex can interpret it. If that still doesn't work, just fall back to the good ol' days when programmers had to code themselves.<br /><br /><span className='TLDR'>TL;DR: CodeXR is great, but not perfect, so make sure your prompt is well written.</span></h3>
+                        <h3>CodeXR uses OpenAIs Codex, an extremely powerful AI System, trained specifically to be able to read and interpret code. BUT, the Codex is also trained on Natural Language, things such as Wikipedia articles, papers and texts, so results should be verbose. However, Codex isn't perfect, so once in a while you may get some unexpected code. If this happens, rewrite your prompt, making sure it provides just enough information that the model can interpret it. If that still doesn't work, just fall back to the good ol' days when programmers had to code themselves.<br /><br /><span className='TLDR'>TL;DR: CodeXR is great, but not perfect, so make sure your prompt is well written.</span></h3>
                     </div>
                 </div>
 
@@ -30,7 +30,7 @@ class AboutPage extends React.Component {
 
                     <div className='row'>
                         <h1>Usage</h1>
-                        <h3>CodeXR is meant to be used with the vscode Insiders build, but a solution for the standard vscode, that CodeXR uses, is to have the completion be put inline directly, instead of as a ghost. This provides the same completion feel as ghost completions, but can be a little buggy sometimes. To account for the bugs, a keybind is put inplace to do the completions whenever they are needed, instead of in realtime. </h3>
+                        <h3>CodeXR can be installed as an extension for the popular code editor, vscode, and to request a completion, a keybind is used.</h3>
                     </div>
                 </div>
             </div>
